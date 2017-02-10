f1259115c1c117862b8d6cfbf1ff735291384bd7
andre <tanandre93@gmail.com>
2017-02-10 01:57:38 -0800

    update user profile information on the fly

--- app.py
+++ app.py
@@ -15,11 +15,14 @@ def _save_credentials(user_id, token):
     if os.path.exists(TOKEN_FILE):
         tokens = json.loads(open(TOKEN_FILE).read())
     if user_id not in tokens:
-        tokens[user_id] = token
+        tokens['users'][user_id] = {
+            'token': token
+        }
+        tokens['changed'] = True
         open(TOKEN_FILE, 'w').write(json.dumps(tokens, indent=4))
 
 def _send_message(recipient, message):
-    tokens = json.loads(open(TOKEN_FILE).read())
+    tokens = _tokens()
     payload = {
         'to': recipient,
         'text': message,
@@ -27,6 +30,24 @@ def _send_message(recipient, message):
     }
     r = requests.post('https://api.flock.co/v1/chat.sendMessage', data=payload)
     print(r.status_code, r.json())
+
+def _tokens():
+    tokens = json.loads(open(TOKEN_FILE).read())
+    if tokens['changed']:
+        for user_id, dic in tokens['users'].iteritems():
+            if len(dic.values()) == 1:
+                r = requests.get('https://api.flock.co/v1/users.getInfo?token={}'.format(dic['token']))
+                user_obj = r.json()
+                tokens['users'][user_id] = {
+                    'first_name': user_obj['firstName'],
+                    'last_name': user_obj['lastName'],
+                    'role': user_obj['role'],
+                    'token': dic['token']
+                }
+                print(r.status_code, tokens['users'][user_id])
+        tokens['changed'] = False
+        open(TOKEN_FILE, 'w').write(json.dumps(tokens, indent=4))
+    return tokens
 
 @app.route("/events", methods=['POST'])
 def events():
@@ -36,7 +57,6 @@ def events():
 
     # auth event when installing app
     if event_name == 'app.install':
-        # user_token = data['userToken']
         token = data['token']
         user_id = data['userId']
         _save_credentials(user_id, token)
@@ -44,7 +64,7 @@ def events():
     # bot events
     elif event_name == 'chat.receiveMessage':
         message = data['message']
-        tokens = json.loads(open(TOKEN_FILE).read())
+        tokens = _tokens()
         if message['from'] in tokens: # only users who have installed the app will be added to the token file
             if message['text'].lower() == 'ping':
                 _send_message(message['from'], 'pong')
