9893883ffda66008ac2fc42f539004bfd834edbf
andre <tanandre93@gmail.com>
2017-02-11 18:22:28 -0800

    send message to group when event received from kyle

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from flask import Flask, render_template, url_for, request
+from flask import Flask, url_for, request
 from gevent.wsgi import WSGIServer
 from settings import *
 from creds import APP_ID, BOT_TOKEN
@@ -25,24 +25,41 @@ def events():
         user_id = data['userId']
         save_credentials(user_id, token)
 
-    elif event_name = 'chat.slashCommand':
+    elif event_name == 'chat.slashCommand':
         # victor
         instructions()
 
-    return event_name
+    elif event_name == 'client.flockmlAction':
+        if 'actionId' in data.keys():
+            if data['actionId'] == 'remind':
+                # victor: implement reminder for 30 minutes
+                print("this fired!")
+                pass
+    return event_name, 200
 
 @app.route("/floqdoc", methods=['POST'])
 def floqdoc():
     try:
         data = json.loads(request.data)
         pprint(data)
-        groups = get_groups()
-        for group in data['assigned_to']:
-            # send attachment to group
-            recipient = groups['by_name'][group]
-            simple_message = Message(to=recipient, text='This works I am so happy')
-            res = flock_client.send_chat(simple_message)
-            print(res)
+        if data['event_name'] == 'question.assign':
+            question_id = data['question_id']
+            asker_id = data['asker']
+            asker_obj = get_tokens(asker_id)
+            asker_name = "{} {}".format(asker_obj['first_name'], asker_obj['last_name'])
+            ask_url = 'https://devweek.kyletan.me/ask/{}'.format(question_id)
+            views = Views()
+            markup = create_flockml(asker_id, asker_name, data['question_title'], ask_url)
+            views.add_flockml(markup)
+
+            groups = get_groups()
+            for group in data['assigned_to']:
+                # send attachment to group
+                recipient = groups['by_name'][group]
+                attachment = Attachment(title='Answer or Remind', views=views)
+                button_message = Message(to = recipient, attachments = [attachment])
+                res = flock_client.send_chat(button_message)
+                print(res)
 
     except ValueError, e:
         return '{} - Try json.dumps.'.format(e), 500
